e8013f8f9f2b4698840c9d2c16514b6ae8f62381
belousmind <belousmind@gmail.com>
2025-02-05 16:34:30 +0300

    feat: add focus on input onload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [query, setQuery] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   console.log(query)
   
   return (
-    <input 
+    <input
+      ref={inputRef}
       type="text"
       value={query}
       placeholder="Search characters..."
