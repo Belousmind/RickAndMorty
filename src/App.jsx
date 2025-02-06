bc2d67627fe7a6fb060fea496e9e55e323035549
belousmind <belousmind@gmail.com>
2025-02-06 15:24:59 +0300

    refactor: split the code into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,14 @@
-import { useState, useRef, useEffect } from 'react';
 import './App.css';
-import CharacterCard from './characterCard/characterCard';
+import SearchCharacter from './components/searchCharacter/searchCharacter';
 
-function App() {
-  const [query, setQuery] = useState("");
-  const inputRef = useRef(null);
-  const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [found, setFound] = useState(true);
-  
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+const App = () => {
 
-  useEffect(() => {
-    if (query.length < 3) {
-      setCharacters([]);
-      setLoading(false);
-      setFound(true);
-      return;
-    }
-  
-    const getCharacters = async () => {
-      setLoading(true);
-  
-      try {
-        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}`);
-  
-        if (!response.ok) {
-          if (response.status === 404) {
-            console.log("Not found");
-            setFound(false);
-            setCharacters([]);
-          } else {
-            console.log(`Could not fetch, status: ${response.status}`);
-            setCharacters([]);
-          }
-        } else {
-          const data = await response.json();
-          setFound(true);
-          setCharacters(data.results || []);
-        }
-      } catch (error) {
-        console.error("Network error:", error);
-        setFound(false);
-        setCharacters([]);
-      } finally {
-        setLoading(false);
-      }
-    };
-  
-    const timeoutId = setTimeout(getCharacters, 500);
-  
-    return () => clearTimeout(timeoutId);
-  
-  }, [query]);
-  
-
-  console.log(characters.length);
-  
   return (
     <main>
-      <input
-        ref={inputRef}
-        type="text"
-        value={query}
-        placeholder="Search characters..."
-        onChange={(e) => setQuery(e.target.value)} />
-
-      {loading && <p>Loading...</p>}
-
-      {!found && <p>Not found! Try another request</p>}
-
-      <div className='cards'>
-        {
-          characters.map(char => {
-            return <CharacterCard key={char.id} {...char}/>
-          })
-        }
-      </div>
+      <SearchCharacter/>
     </main>
   )
 }
 
 export default App;
-
-// id, name, species, status, created, url  
+ 
